bc585fae502d59233bcebccf420be2da294dd04b
AlionaAndruschenko <alionaandruschenko@gmail.com>
2026-03-25 22:57:13 +0200

    Create haskell_1.hs

---
+++ Haskell/haskell_1.hs
@@ -0,0 +1,25 @@
+removeSingles :: Eq a => [a] -> [a]
+removeSingles xs = [x | x <- xs, getCount x counts /= 1]
+  where
+    counts = buildCounts xs
+
+buildCounts :: Eq a => [a] -> [(a, Int)]
+buildCounts [] = []
+buildCounts (x:xs) = addCount x (buildCounts xs)
+
+addCount :: Eq a => a -> [(a, Int)] -> [(a, Int)]
+addCount x [] = [(x,1)]
+addCount x ((y,n):ys)
+    | x == y    = (y,n+1):ys
+    | otherwise = (y,n) : addCount x ys
+
+getCount :: Eq a => a -> [(a, Int)] -> Int
+getCount _ [] = 0
+getCount x ((y,n):ys)
+    | x == y    = n
+    | otherwise = getCount x ys
+
+main :: IO ()
+main = do
+    let xs = [1, 2, 3, 2, 4, 3, 5, 1, 6, 7, 6, 8, 9, 9, 9]
+    print (removeSingles xs)
